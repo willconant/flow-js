756ca8f3e2e2840ad24e144129da5973acd4be27
Will Conant <will.conant@gmail.com>
2010-03-06 20:24:34 -0700

    added spooling

--- flow.js
+++ flow.js
@@ -1,55 +1,61 @@
-function toArray(args) {
-	return Array.prototype.slice.call(args);
-}
+// Javascript Library for Multi-step Asynchronous Jobs
+// Version 0.1
+// Copyright (c) 2010 William R. Conant, WillConant.com
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
-function applyArgs(func, thisObj, args) {
-	return func.apply(thisObj, toArray(args));
-}
-
-exports.define = function() {	
-	var thisFlow = function() {
-		applyArgs(thisFlow.exec, thisFlow, arguments);
+(function(){
+	// converts native arguments object to an array and applies function
+	function applyArgs(func, thisObj, args) {
+		return func.apply(thisObj, Array.prototype.slice.call(args));
 	}
 	
-	thisFlow.blocks = arguments;
-	
-	thisFlow.exec = function() {
-		var flowState = function() {
-			var blockIdx = flowState.nextBlockIdx ++;
-			var block = thisFlow.blocks[blockIdx];
-			
-			if (block === undefined) {
-				return;
-			}
-			else {
-				applyArgs(block, flowState, arguments);
-			}
+	// defines a flow given any number of functions as arguments
+	exports.define = function() {	
+		var thisFlow = function() {
+			applyArgs(thisFlow.exec, thisFlow, arguments);
 		}
 		
-		flowState.nextBlockIdx = 0;
-		flowState.spoolCount = 0;
-		flowState.spoolOutputs = [];
+		thisFlow.blocks = arguments;
 		
-		flowState.spool = function() {
-			flowState.spoolCount += 1;
-			return function() {
-				flowState.spoolCount -= 1;
-				flowState.spoolOutputs.push(arguments);
+		thisFlow.exec = function() {
+			var flowState = function() {
+				var blockIdx = flowState.nextBlockIdx ++;
+				var block = thisFlow.blocks[blockIdx];
 				
-				if (flowState.spoolCount == 0) {
-					var spoolOutputs = flowState.spoolOutputs;
-					flowState.spoolOutputs = [];
-					flowState(spoolOutputs);
+				if (block === undefined) {
+					return;
+				}
+				else {
+					applyArgs(block, flowState, arguments);
 				}
 			}
+			
+			flowState.nextBlockIdx = 0;
+			flowState.spoolCount = 0;
+			flowState.spoolOutputs = [];
+			
+			flowState.spool = function() {
+				flowState.spoolCount += 1;
+				return function() {
+					flowState.spoolCount -= 1;
+					flowState.spoolOutputs.push(arguments);
+					
+					if (flowState.spoolCount == 0) {
+						var spoolOutputs = flowState.spoolOutputs;
+						flowState.spoolOutputs = [];
+						flowState(spoolOutputs);
+					}
+				}
+			}
+			
+			applyArgs(flowState, this, arguments);
 		}
 		
-		applyArgs(flowState, this, arguments);
+		return thisFlow;
 	}
 	
-	return thisFlow;
-}
-
-exports.exec = function() {
-	applyArgs(exports.define, exports, arguments)();
-}
+	// defines a flow and evaluates it immediately. The first flow function won't receive any arguments.
+	exports.exec = function() {
+		applyArgs(exports.define, exports, arguments)();
+	}
+})();
